bb154f5fdbfe842f3ec46c9a1daaa4fe15a4a481
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-03-27 23:09:13 +0530

    Added Slider in notes view

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:mynotes/service/cloud/cloud_notes.dart';
+import 'package:share_plus/share_plus.dart';
 import '../../utilities/dialog/delete_dialog.dart';
 
 typedef NotesCallback = void Function(CloudNotes note);
@@ -22,24 +24,67 @@ class NoteListView extends StatelessWidget {
       itemCount: notes.length,
       itemBuilder: (context, index) {
         final note = notes.elementAt(index);
-        return ListTile(
-          onTap: () {
-            onTap(note);
-          },
-          title: Text(
-            note.text,
-            maxLines: 1,
-            softWrap: true,
-            overflow: TextOverflow.ellipsis,
+        return Slidable(
+          key: ValueKey(note.documentId),
+          startActionPane: ActionPane(
+            motion: const BehindMotion(),
+            children: [
+              SlidableAction(
+                label: "Delete",
+                onPressed: (context) async {
+                  final shouldDelete = await showDeleteDialog(context);
+                  if (shouldDelete) {
+                    onDeleteNote(note);
+                  }
+                },
+                icon: Icons.delete,
+                backgroundColor: Colors.red,
+              ),
+            ],
           ),
-          trailing: IconButton(
-            onPressed: () async {
-              final shouldDelete = await showDeleteDialog(context);
-              if (shouldDelete) {
-                onDeleteNote(note);
-              }
-            },
-            icon: const Icon(Icons.delete),
+          endActionPane: ActionPane(
+            motion: const BehindMotion(),
+            children: [
+              SlidableAction(
+                label: "Share",
+                onPressed: (context) async {
+                  Share.share(note.text);
+                },
+                icon: Icons.share,
+                backgroundColor: Colors.green,
+              ),
+              SlidableAction(
+                label: "Delete",
+                onPressed: (context) async {
+                  final shouldDelete = await showDeleteDialog(context);
+                  if (shouldDelete) {
+                    onDeleteNote(note);
+                  }
+                },
+                icon: Icons.delete,
+                backgroundColor: Colors.red,
+              ),
+            ],
+          ),
+          child: Card(
+            child: ListTile(
+              onTap: () => onTap(note),
+              title: Text(
+                note.text,
+                maxLines: 1,
+                softWrap: true,
+                overflow: TextOverflow.ellipsis,
+              ),
+              trailing: IconButton(
+                onPressed: () async {
+                  final shouldDelete = await showDeleteDialog(context);
+                  if (shouldDelete) {
+                    onDeleteNote(note);
+                  }
+                },
+                icon: const Icon(Icons.delete),
+              ),
+            ),
           ),
         );
       },
